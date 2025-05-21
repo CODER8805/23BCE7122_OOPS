bd00570802ba5d871ee3214e4247e2938dee2db4
CODER8805 <159150009+CODER8805@users.noreply.github.com>
2025-05-21 18:26:44 +0530

    Update SortArrayAscending.java

--- LAB1_23BCE7122/SortArrayAscending.java
+++ LAB1_23BCE7122/SortArrayAscending.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class SortArrayAscending {
     public static void main(String[] args) {
-        // Declare and initialize the array
+        
         int[] arr = {5, 2, 9, 1, 7};
 
-        // Sort the array in ascending order
+        
         Arrays.sort(arr);
 
-        // Print the sorted array
+        
         System.out.print("Sorted array in ascending order: ");
         for (int num : arr) {
             System.out.print(num + " ");
