e13351a9bc3d19906cb376c25d37f166c76e1213
CODER8805 <159150009+CODER8805@users.noreply.github.com>
2025-06-09 23:07:52 +0530

    lab6

---
+++ OOPs_lab6/tempCodeRunnerFile.java
@@ -0,0 +1,41 @@
+//write  a code to build a student management system that stores marks of 5 students
+// and calculate total and average
+//use collections
+
+import java.util.*;
+
+class calculator{
+    public static int total(ArrayList<Integer> list){
+        int total = 0;
+        for(int i = 0;i<list.size();i++){
+            total+=list.get(i);
+
+        }
+        return total;
+    }
+
+    public static void average(ArrayList<Integer> list){
+        int average = total(list)/list.size();
+        System.out.println("the average of students marks is :"+ average);
+        
+    }
+
+}
+
+public class Main {
+    public static void Main(String[] args){
+        ArrayList<Integer> Student = new ArrayList<>();
+        Student.add(98);
+        Student.add(95);
+        Student.add(96);
+        Student.add(92);
+        Student.add(99);
+
+        calculator c = new calculator();
+        c.total(Student);
+        c.average(Student);
+
+
+    }
+    
+}
